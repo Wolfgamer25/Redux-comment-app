bcb28fadc55da72d456c89e2ad3c9b36db809d62
Jonathan Torres <Jonathan.torres25@live.com>
2018-03-06 21:43:28 -0500

    finished functionality, add redux routes, add post and delete post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from  'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
 class PostsNew extends Component {
   renderField(field){
     const { meta: {touched, error} } = field;
@@ -22,7 +25,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values){
-    console.log(values )
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render(){
@@ -72,4 +77,6 @@ function validate(values){
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, {createPost})(PostsNew)
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POST, FETCH_POSTS, FETCH_DELETE } from '../actions';
 
 export default function(state = {}, action){
   switch(action.type){
+    case FETCH_DELETE:
+    return _.omit(state, action.payload);
+
+    case FETCH_POST:
+      return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
